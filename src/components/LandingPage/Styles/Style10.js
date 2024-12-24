4915ed30cff0708ae6fd6eaafb10e5fb36ba1433
krazykarthik2 <goparajukarthik2@gmail.com>
2024-12-24 20:22:18 +0530

    perfected word type

--- src/components/LandingPage/Styles/Style10.js
+++ src/components/LandingPage/Styles/Style10.js
@@ -8,7 +8,9 @@ import {
   selectTypingSettings,
   setTapeModeMovePref,
   setTypingMode,
+  setWords,
   tapeModeOptions,
+  wordOptions,
 } from "../../../redux/typingSlice";
 import { getRandomQuote, getRandomWords } from "../../../util/jsutil";
 import KeyboardMapper from "../../KeyboardMapper/KeyboardMapper";
@@ -17,9 +19,16 @@ import { alternateDiff, Cursor } from "../../TypingPage/TypingPage";
 import { getFormattedText, withoutLast } from "../../TypingPage/util";
 import { GithubBtn, LoggedinAs, ShareBtn, VersionBtn } from "../pins";
 import "./Style10.css";
-function Diff({ source, change }) {
+function Diff({
+  source,
+  change,
+  fadeOut = false,
+  readyFlag = true,
+  setReadyFlag = () => {},
+}) {
   const [list, diff_list] = alternateDiff(source, change);
   const list_spaced = list.map((e) => e?.split(" "));
+
   return (
     source.length != change.length || (
       <div className="flex d-center gap-0">
@@ -28,32 +37,34 @@ function Diff({ source, change }) {
             <div className="d-center gap-2" key={i}>
               {list_spaced[i].map(
                 // word:1 word:last has no margin
-                (ee, ii) => (
-                  <div
-                    className={
-                      "before-diff-" +
-                      (ii == 0) +
-                      " after-diff-" +
-                      (ii == list_spaced[i].length - 1)
-                    }
-                    key={ii}
-                  >
-                    {ee?.split().map((eee, iii) => (
-                      <span
-                        key={iii}
-                        className={
-                          diff_list[i] === null
-                            ? "text-gray-400"
-                            : diff_list[i]
-                            ? "text-green-400"
-                            : "text-red-400"
-                        }
-                      >
-                        {eee}
-                      </span>
-                    ))}
-                  </div>
-                )
+                (ee, ii) => {
+                  return (
+                    <div
+                      className={
+                        " before-diff-" +
+                        (ii == 0) +
+                        " after-diff-" +
+                        (ii == list_spaced[i].length - 1)
+                      }
+                      key={ii}
+                    >
+                      {ee?.split().map((eee, iii) => (
+                        <span
+                          key={iii}
+                          className={
+                            diff_list[i] === null
+                              ? "text-gray-400"
+                              : diff_list[i]
+                              ? "text-green-400"
+                              : "text-red-400"
+                          }
+                        >
+                          {eee}
+                        </span>
+                      ))}
+                    </div>
+                  );
+                }
               )}
             </div>
           );
@@ -62,6 +73,9 @@ function Diff({ source, change }) {
     )
   );
 }
+
+const BEFORELIMIT = 25,
+  AFTERLIMIT = 25;
 const TypeWriter = () => {
   const inputRef = React.useRef(null);
 
@@ -157,77 +171,85 @@ const TypeWriter = () => {
       rawWpm: raw_wpm,
     };
   }, [input, startTime, text]);
+
+  const BEFORETEXT = text?.substring(input.length - BEFORELIMIT, input.length);
+  const BEFORETEXTLASTWORD = BEFORETEXT?.split(" ").at(-1);
+  const BEFOREINPUTLASTWORD = input?.substring(
+    input.length - BEFORETEXTLASTWORD.length,
+    input.length
+  );
+  const BEFORETEXTWITHOUTLASTWORD = withoutLast(BEFORETEXT?.split(" ")).join(
+    " "
+  );
+  const BEFOREINPUTWITHOUTLASTWORD = input?.substring(
+    input.length - BEFORELIMIT,
+    input.length - 1 - BEFORETEXTLASTWORD.length
+  );
+  const INPUTATLASTWORDINTERVAL = input.at(-1 - BEFORETEXTLASTWORD.length);
+  const BEFOREINPUT = input?.substring(
+    input.length - BEFORELIMIT,
+    input.length
+  );
+  const AFTERTEXT = text?.substring(
+    input.length,
+    input.length + AFTERLIMIT - BEFOREINPUTLASTWORD.length
+  );
   return (
     <div className="d-center h-full w-full">
       {
         <React.Fragment key={move && input.length}>
           <div
             className={
-              "d-center w-full gap-0 max-h-full text-4xl " +" animation-to-left"}
+              "d-center w-[fit-content] gap-0 max-h-full text-4xl overflow-visible"
+            }
           >
             {move == "letter" && (
               <>
                 <div className="w-full flex justify-end">
                   <Diff
-                    source={text?.substring(input.length - 30, input.length)}
-                    change={input?.substring(input.length - 30, input.length)}
+                    source={BEFORETEXT}
+                    change={BEFOREINPUT}
+                    fadeOut={true}
                   />
                 </div>
                 {input.at(-1) == " " && <div className="ms-2" />}
                 <Cursor />
                 <pre className="w-full flex justify-start flex-nowrap nowrap text-gray-500">
                   {/* {text.at(input.length) == " " && <>&nbsp;</>} */}
-                  {text?.substring(input.length, input.length + 30)}
+                  {AFTERTEXT}
                 </pre>
               </>
             )}
             {move == "word" && (
               <>
-                <div className="w-full flex justify-end">
+                <div className="w-fit-content flex justify-end">
+                  {BEFORELIMIT<input.length && Array(BEFORETEXTLASTWORD.length).fill(<>&nbsp;</>)}
                   <Diff
-                    source={withoutLast(
-                      text
-                        ?.substring(input.length - 30, input.length)
-                        ?.split(" ")
-                    )?.join(" ")}
-                    change={input?.substring(
-                      input.length - 30,
-                      input.length -
-                        1 -
-                        text
-                          ?.substring(input.length - 30, input.length)
-                          ?.split(" ")
-                          ?.at(-1).length
-                    )}
+                    source={BEFORETEXTWITHOUTLASTWORD}
+                    change={BEFOREINPUTWITHOUTLASTWORD}
+                    fadeOut={true}
                   />
                 </div>
-                <div
-                  className={
-                    "flex justify-start items-center " +
-                    (input?.includes(" ") ? "ms-3" : "")
-                  }
-                >
+                <div className={"flex justify-start items-center scale-1.2"}>
+                  {INPUTATLASTWORDINTERVAL == " " ? (
+                    <>&nbsp;</>
+                  ) : (
+                    <span className="text-red-400">
+                      {INPUTATLASTWORDINTERVAL}
+                    </span>
+                  )}
+
                   <Diff
-                    source={text
-                      ?.substring(input.length - 30, input.length)
-                      ?.split(" ")
-                      ?.at(-1)}
-                    change={input?.substring(
-                      input.length -
-                        text
-                          ?.substring(input.length - 30, input.length)
-                          ?.split(" ")
-                          ?.at(-1).length,
-                      input.length
-                    )}
+                    source={BEFORETEXTLASTWORD}
+                    change={BEFOREINPUTLASTWORD}
+                    fadeOut={false}
                   />
                   {input.at(-1) == " " && <div className="ms-2" />}
                   <Cursor />
                 </div>
-                <div className="d-center w-full">
+                <div className="d-center w-fit-content">
                   <pre className="w-full flex justify-start flex-nowrap nowrap text-gray-500">
-                    {/* {text.at(input.length) == " " && <>&nbsp;</>} */}
-                    {text?.substring(input.length, input.length + 30)}
+                    {AFTERTEXT}
                   </pre>
                 </div>
               </>
@@ -274,46 +296,69 @@ const Style10 = ({ hours, minutes, props, dispatch }) => {
             <Terminal {...props} hidden={!terminal} />
             {!terminal && <TypeWriter />}{" "}
           </div>
-          <div className="d-center settings px-10 py-5 gap-10">
-            <div className="d-center rounded-lg bg-gray-800 px-3 py-1 gap-10">
-              <button
-                onClick={() => setTerminal(true)}
-                className={terminal ? "text-red-500" : "text-gray-500"}
-              >
-                Terminal
-              </button>
-              <button
-                onClick={() => setTerminal(false)}
-                className={!terminal ? "text-red-500" : "text-gray-500"}
-              >
-                Typer
-              </button>
-            </div>
-            <div className="d-center rounded-lg bg-gray-800 px-3 py-1 gap-10">
-              {tapeModeOptions.map((e) => (
+        
+          <div className="stack d-center">
+          {!terminal &&
+            <div className="d-center settings px-10 py-5 gap-10">
+              <div className="d-center rounded-lg bg-gray-800 px-3 py-1 gap-10">
+                {tapeModeOptions.map((e) => (
+                  <button
+                    key={e}
+                    onClick={() => dispatch(setTapeModeMovePref(e))}
+                    className={move == e ? "text-red-500" : "text-gray-500"}
+                  >
+                    {e}
+                  </button>
+                ))}
+              </div>
+              <div className="d-center rounded-lg bg-gray-800 px-3 py-1 gap-10">
+                {modeOptions.map((e) => (
+                  <button
+                    disabled={e == "timeLimit"}
+                    key={e}
+                    onClick={() => dispatch(setTypingMode(e))}
+                    className={
+                      "disabled:text-black " +
+                      (mode == e ? "text-red-500" : "text-gray-500")
+                    }
+                  >
+                    {e}
+                  </button>
+                ))}
+              </div>
+              {mode == "words" && (
+                <div className="d-center rounded-lg bg-gray-800 px-3 py-1 gap-10">
+                  {wordOptions.map((e) => (
+                    <button
+                      disabled={e == "timeLimit"}
+                      key={e}
+                      onClick={() => dispatch(setWords(e))}
+                      className={
+                        "disabled:text-black " +
+                        (words == e ? "text-red-500" : "text-gray-500")
+                      }
+                    >
+                      {e}
+                    </button>
+                  ))}
+                </div>
+              )}
+            </div>}
+            <div className="d-center opts">
+              <div className="d-center rounded-lg bg-gray-800 px-3 py-1 gap-10">
                 <button
-                  key={e}
-                  onClick={() => dispatch(setTapeModeMovePref(e))}
-                  className={move == e ? "text-red-500" : "text-gray-500"}
+                  onClick={() => setTerminal(true)}
+                  className={terminal ? "text-red-500" : "text-gray-500"}
                 >
-                  {e}
+                  Terminal
                 </button>
-              ))}
-            </div>
-            <div className="d-center rounded-lg bg-gray-800 px-3 py-1 gap-10">
-              {modeOptions.map((e) => (
                 <button
-                  disabled={e == "timeLimit"}
-                  key={e}
-                  onClick={() => dispatch(setTypingMode(e))}
-                  className={
-                    "disabled:text-black " +
-                    (mode == e ? "text-red-500" : "text-gray-500")
-                  }
+                  onClick={() => setTerminal(false)}
+                  className={!terminal ? "text-red-500" : "text-gray-500"}
                 >
-                  {e}
+                  Typer
                 </button>
-              ))}
+              </div>
             </div>
           </div>
           <div className="github">
